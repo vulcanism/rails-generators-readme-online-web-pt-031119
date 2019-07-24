0d094b3925ebd5a0d06da44f80b9e745a72cdf2d
Savannah C <sgcorrell@gmail.com>
2019-07-24 20:37:21 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190724203627) do
+ActiveRecord::Schema.define(version: 20190724203714) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190724203627) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
